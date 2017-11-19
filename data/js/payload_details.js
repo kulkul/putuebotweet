d3f49055682c3ab7fa4045c61e767b059a709000
kulkul <e_bowis@yahoo.com>
2017-11-19 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 7586,
-	"created_at": "Sat Nov 18 01:14:56 +0000 2017",
+	"tweets": 7587,
+	"created_at": "Sun Nov 19 01:14:51 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Sat Nov 18 05:39:56 +0000 2017",
+		"id": 931758854152450000,
+		"id_str": "931758854152450048",
+		"text": "RT @NissanID: Menikung dengan kecepatan tinggi lebih presisi, terkendali, tanpa risiko keluar jalur berkat teknologi Active Trace Control d…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NissanID",
+					"name": "NISSAN Indonesia",
+					"id": 69166306,
+					"id_str": "69166306",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1371,
+			"friends_count": 888,
+			"listed_count": 17,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 2324,
+			"utc_offset": 28800,
+			"time_zone": "Ulaan Bataar",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7584,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/739898263/db3e67b3b4be6e9123616cb237a46315.png",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/739898263/db3e67b3b4be6e9123616cb237a46315.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Nov 17 16:08:14 +0000 2017",
+			"id": 931554580269187100,
+			"id_str": "931554580269187072",
+			"text": "Menikung dengan kecepatan tinggi lebih presisi, terkendali, tanpa risiko keluar jalur berkat teknologi Active Trace… https://t.co/Aoan6L452g",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Aoan6L452g",
+						"expanded_url": "https://twitter.com/i/web/status/931554580269187072",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 69166306,
+				"id_str": "69166306",
+				"name": "NISSAN Indonesia",
+				"screen_name": "NissanID",
+				"location": "Jakarta, Indonesia",
+				"description": "Akun resmi PT Nissan Motors Indonesia. Follow kami untuk berita & promo terbaru mengenai Nissan. Ikuti juga Facebook kami di http://t.co/32PGhYRG",
+				"url": "http://t.co/l9zig2DcJn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/l9zig2DcJn",
+								"expanded_url": "http://www.nissan.co.id",
+								"display_url": "nissan.co.id",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "http://t.co/32PGhYRG",
+								"expanded_url": "http://facebook.com/NissanZoneIndonesia",
+								"display_url": "facebook.com/NissanZoneIndo…",
+								"indices": [
+									125,
+									145
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 57947,
+				"friends_count": 921,
+				"listed_count": 166,
+				"created_at": "Thu Aug 27 01:49:07 +0000 2009",
+				"favourites_count": 382,
+				"utc_offset": 25200,
+				"time_zone": "Jakarta",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 31268,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/577096770/1r287ek05ckhtzehmirb.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/577096770/1r287ek05ckhtzehmirb.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/420081954231287808/Fp7unby6_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/420081954231287808/Fp7unby6_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/69166306/1508926601",
+				"profile_link_color": "C51733",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2382,
+			"favorite_count": 1230,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "in"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2382,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "in"
+	},
+	{
 		"created_at": "Fri Nov 17 08:55:39 +0000 2017",
 		"id": 931445716999839700,
 		"id_str": "931445716999839745",
