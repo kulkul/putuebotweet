a69f2a67ed67bc10c28614beb1040b440e9acf94
kulkul <e_bowis@yahoo.com>
2018-11-04 09:14:51 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8505,
-	"created_at": "Sat Nov 03 01:14:51 +0000 2018",
+	"tweets": 8506,
+	"created_at": "Sun Nov 04 01:14:51 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -115,6 +115,167 @@ Grailbird.data.tweets_2018_11 =
 			},
 			"protected": false,
 			"followers_count": 362,
+			"friends_count": 548,
+			"listed_count": 2,
+			"created_at": "Thu Apr 08 13:58:54 +0000 2010",
+			"favourites_count": 3731,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3683,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "020614",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/942158526431895552/DLe8NLz9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/942158526431895552/DLe8NLz9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/130843813/1534084139",
+			"profile_link_color": "E3D216",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "EB174C",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "cy",
+		"text": "Bohemian Rhapsody https://t.co/ZigvSzlo79"
+	},
+	{
+		"created_at": "Thu Nov 01 22:28:03 +0000 2018",
+		"id": 1058123529256808400,
+		"id_str": "1058123529256808448",
+		"full_text": "Bohemian Rhapsody https://t.co/ZigvSzlo79",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1058123520918532100,
+					"id_str": "1058123520918532097",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"url": "https://t.co/ZigvSzlo79",
+					"display_url": "pic.twitter.com/ZigvSzlo79",
+					"expanded_url": "https://twitter.com/YuanitaRamadhan/status/1058123529256808448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058123520918532100,
+					"id_str": "1058123520918532097",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"url": "https://t.co/ZigvSzlo79",
+					"display_url": "pic.twitter.com/ZigvSzlo79",
+					"expanded_url": "https://twitter.com/YuanitaRamadhan/status/1058123529256808448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 130843813,
+			"id_str": "130843813",
+			"name": "Yuanita Ramadhani",
+			"screen_name": "YuanitaRamadhan",
+			"location": "Indonesia",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 362,
 			"friends_count": 546,
 			"listed_count": 2,
 			"created_at": "Thu Apr 08 13:58:54 +0000 2010",
