6f1cc532fb2fa6f14a19f92f63e10b47f95e3d25
kulkul <e_bowis@yahoo.com>
2018-11-06 09:14:52 +0800

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8507,
-	"created_at": "Mon Nov 05 01:14:51 +0000 2018",
+	"tweets": 8510,
+	"created_at": "Tue Nov 06 01:14:52 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,729 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Mon Nov 05 10:49:00 +0000 2018",
+		"id": 1059397162679713800,
+		"id_str": "1059397162679713793",
+		"full_text": "3 juara #GNRJKTPosterCompetition source: IG https://t.co/PDmwMWa4VG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GNRJKTPosterCompetition",
+					"indices": [
+						8,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1059396768931098600,
+					"id_str": "1059396768931098624",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DrO7v5hVsAAhIcY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrO7v5hVsAAhIcY.jpg",
+					"url": "https://t.co/PDmwMWa4VG",
+					"display_url": "pic.twitter.com/PDmwMWa4VG",
+					"expanded_url": "https://twitter.com/putuebo/status/1059397162679713793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059396768931098600,
+					"id_str": "1059396768931098624",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DrO7v5hVsAAhIcY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrO7v5hVsAAhIcY.jpg",
+					"url": "https://t.co/PDmwMWa4VG",
+					"display_url": "pic.twitter.com/PDmwMWa4VG",
+					"expanded_url": "https://twitter.com/putuebo/status/1059397162679713793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1059396994890817500,
+					"id_str": "1059396994890817536",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DrO79DSVYAAatcL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrO79DSVYAAatcL.jpg",
+					"url": "https://t.co/PDmwMWa4VG",
+					"display_url": "pic.twitter.com/PDmwMWa4VG",
+					"expanded_url": "https://twitter.com/putuebo/status/1059397162679713793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1059397033717448700,
+					"id_str": "1059397033717448704",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DrO7_T7UwAAO6x_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrO7_T7UwAAO6x_.jpg",
+					"url": "https://t.co/PDmwMWa4VG",
+					"display_url": "pic.twitter.com/PDmwMWa4VG",
+					"expanded_url": "https://twitter.com/putuebo/status/1059397162679713793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1435,
+			"friends_count": 930,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 6061,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8467,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "3 juara #GNRJKTPosterCompetition source: IG https://t.co/PDmwMWa4VG"
+	},
+	{
+		"created_at": "Mon Nov 05 10:38:45 +0000 2018",
+		"id": 1059394580049588200,
+		"id_str": "1059394580049588225",
+		"full_text": "Horeeeee.... Selamat @yoshi_amtha jadi pemenang lomba poster #GNRJKTPosterCompetition #makanmakan https://t.co/NmOlSgIAx9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GNRJKTPosterCompetition",
+					"indices": [
+						61,
+						85
+					]
+				},
+				{
+					"text": "makanmakan",
+					"indices": [
+						86,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "yoshi_amtha",
+					"name": "yoshi amtha",
+					"id": 41992274,
+					"id_str": "41992274",
+					"indices": [
+						21,
+						33
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NmOlSgIAx9",
+					"expanded_url": "https://twitter.com/yoshi_amtha/status/1058721198627004417",
+					"display_url": "twitter.com/yoshi_amtha/st…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1435,
+			"friends_count": 930,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 6061,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8467,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1058721198627004400,
+		"quoted_status_id_str": "1058721198627004417",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NmOlSgIAx9",
+			"expanded": "https://twitter.com/yoshi_amtha/status/1058721198627004417",
+			"display": "twitter.com/yoshi_amtha/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Nov 03 14:02:58 +0000 2018",
+			"id": 1058721198627004400,
+			"id_str": "1058721198627004417",
+			"full_text": "Thanks to gnrjkt and @temgmt to choose my design as one of top 3 winners in #GNRJKTPosterCompetition \n\nYou ROCK!! @ Denpasar, Bali, Indonesia https://t.co/bjljUAXK1D",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				165
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GNRJKTPosterCompetition",
+						"indices": [
+							76,
+							100
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "TEMGMT",
+						"name": "Third Eye Management",
+						"id": 817472594,
+						"id_str": "817472594",
+						"indices": [
+							21,
+							28
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/bjljUAXK1D",
+						"expanded_url": "https://www.instagram.com/p/BpuIg5wAym8/?utm_source=ig_twitter_share&igshid=1c3b0g7n04egc",
+						"display_url": "instagram.com/p/BpuIg5wAym8/…",
+						"indices": [
+							142,
+							165
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 41992274,
+				"id_str": "41992274",
+				"name": "yoshi amtha",
+				"screen_name": "yoshi_amtha",
+				"location": "Singapore",
+				"description": "I love drawing with my sons | batturandu@gmail.com",
+				"url": "https://t.co/TOQbzBGLLd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TOQbzBGLLd",
+								"expanded_url": "http://batturandu.jimdo.com",
+								"display_url": "batturandu.jimdo.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 553,
+				"friends_count": 366,
+				"listed_count": 19,
+				"created_at": "Sat May 23 07:18:25 +0000 2009",
+				"favourites_count": 296,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4298,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/908539782501773312/UaKmXEgr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/908539782501773312/UaKmXEgr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/41992274/1475833538",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": {
+				"type": "Point",
+				"coordinates": [
+					-8.6764,
+					115.2153
+				]
+			},
+			"coordinates": {
+				"type": "Point",
+				"coordinates": [
+					115.2153,
+					-8.6764
+				]
+			},
+			"place": {
+				"id": "cf712fd91280ebff",
+				"url": "https://api.twitter.com/1.1/geo/id/cf712fd91280ebff.json",
+				"place_type": "city",
+				"name": "Denpasar Barat",
+				"full_name": "Denpasar Barat, Indonesia",
+				"country_code": "ID",
+				"country": "Indonesia",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								115.173695,
+								-8.70934
+							],
+							[
+								115.235563,
+								-8.70934
+							],
+							[
+								115.235563,
+								-8.591728
+							],
+							[
+								115.173695,
+								-8.591728
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Horeeeee.... Selamat @yoshi_amtha jadi pemenang lomba poster #GNRJKTPosterCompetition #makanmakan https://t.co/NmOlSgIAx9"
+	},
+	{
+		"created_at": "Thu Nov 01 22:28:03 +0000 2018",
+		"id": 1058123529256808400,
+		"id_str": "1058123529256808448",
+		"full_text": "Bohemian Rhapsody https://t.co/ZigvSzlo79",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1058123520918532100,
+					"id_str": "1058123520918532097",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"url": "https://t.co/ZigvSzlo79",
+					"display_url": "pic.twitter.com/ZigvSzlo79",
+					"expanded_url": "https://twitter.com/YuanitaRamadhan/status/1058123529256808448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058123520918532100,
+					"id_str": "1058123520918532097",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dq81vHDVsAEmToE.jpg",
+					"url": "https://t.co/ZigvSzlo79",
+					"display_url": "pic.twitter.com/ZigvSzlo79",
+					"expanded_url": "https://twitter.com/YuanitaRamadhan/status/1058123529256808448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 717,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 130843813,
+			"id_str": "130843813",
+			"name": "Yuanita Ramadhani",
+			"screen_name": "YuanitaRamadhan",
+			"location": "Indonesia",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 362,
+			"friends_count": 551,
+			"listed_count": 2,
+			"created_at": "Thu Apr 08 13:58:54 +0000 2010",
+			"favourites_count": 3751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3687,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "020614",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/942158526431895552/DLe8NLz9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/942158526431895552/DLe8NLz9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/130843813/1534084139",
+			"profile_link_color": "E3D216",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "EB174C",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "cy",
+		"text": "Bohemian Rhapsody https://t.co/ZigvSzlo79"
+	},
+	{
 		"created_at": "Thu Nov 01 22:28:03 +0000 2018",
 		"id": 1058123529256808400,
 		"id_str": "1058123529256808448",
