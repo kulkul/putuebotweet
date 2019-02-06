ba124a527a1f610fd63aed7d2d0b8799571db33f
kulkul <e_bowis@yahoo.com>
2019-02-06 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8643,
-	"created_at": "Tue Feb 05 01:14:51 +0000 2019",
+	"tweets": 8644,
+	"created_at": "Wed Feb 06 01:14:52 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 05 12:30:16 +0000 2019",
+		"id": 1092762329651892200,
+		"id_str": "1092762329651892224",
+		"full_text": "Cobalah klik hastag #whiteudengcrime di FB. Isu ini serius, jika kita mencintai Bali. https://t.co/UsOgfkH288",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "whiteudengcrime",
+					"indices": [
+						20,
+						36
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092762318557962200,
+					"id_str": "1092762318557962240",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/DypFif2UYAAVMw-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DypFif2UYAAVMw-.jpg",
+					"url": "https://t.co/UsOgfkH288",
+					"display_url": "pic.twitter.com/UsOgfkH288",
+					"expanded_url": "https://twitter.com/putuebo/status/1092762329651892224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 483,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092762318557962200,
+					"id_str": "1092762318557962240",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/DypFif2UYAAVMw-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DypFif2UYAAVMw-.jpg",
+					"url": "https://t.co/UsOgfkH288",
+					"display_url": "pic.twitter.com/UsOgfkH288",
+					"expanded_url": "https://twitter.com/putuebo/status/1092762329651892224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 483,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1423,
+			"friends_count": 941,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 7409,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8585,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9e7fa66c85000",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9e7fa66c85000.json",
+			"place_type": "poi",
+			"name": "Studio Kartun Putu Ebo",
+			"full_name": "Studio Kartun Putu Ebo",
+			"country_code": "ID",
+			"country": "Indonesia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.22567957639694,
+							-8.615039253721804
+						],
+						[
+							115.22567957639694,
+							-8.615039253721804
+						],
+						[
+							115.22567957639694,
+							-8.615039253721804
+						],
+						[
+							115.22567957639694,
+							-8.615039253721804
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Cobalah klik hastag #whiteudengcrime di FB. Isu ini serius, jika kita mencintai Bali. https://t.co/UsOgfkH288"
+	},
+	{
 		"created_at": "Mon Feb 04 12:27:27 +0000 2019",
 		"id": 1092399231371857900,
 		"id_str": "1092399231371857920",
