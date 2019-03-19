9c5aa949786859ce428ae9f13d76eeff13d13695
kulkul <e_bowis@yahoo.com>
2019-03-19 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8780,
-	"created_at": "Sun Mar 17 01:14:51 +0000 2019",
+	"tweets": 8781,
+	"created_at": "Tue Mar 19 01:14:52 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 18 12:07:30 +0000 2019",
+		"id": 1107614502533517300,
+		"id_str": "1107614502533517313",
+		"full_text": "@makmummasjid Mudra pada dasarnya adalah gesture ekspresi pd tangan. Tangan melambai saat menyapa, unjuk jempol saat setuju, mendekap dada untuk nyaman, mengenggam saat atur emosi, bbrp budaya menemukan bentuk mudra untuk mencapai konsentrasi &amp; energi. Masalah ada kalau dipermasalahkan.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			291
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "makmummasjid",
+					"name": "renné nésa",
+					"id": 103521467,
+					"id_str": "103521467",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1107552965101936600,
+		"in_reply_to_status_id_str": "1107552965101936640",
+		"in_reply_to_user_id": 103521467,
+		"in_reply_to_user_id_str": "103521467",
+		"in_reply_to_screen_name": "makmummasjid",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1427,
+			"friends_count": 943,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 7954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8687,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "in",
+		"text": "@makmummasjid Mudra pada dasarnya adalah gesture ekspresi pd tangan. Tangan melambai saat menyapa, unjuk jempol saat setuju, mendekap dada untuk nyaman, mengenggam saat atur emosi, bbrp budaya menemukan bentuk mudra untuk mencapai konsentrasi &amp; energi. Masalah ada kalau dipermasalahkan."
+	},
+	{
 		"created_at": "Sun Mar 17 01:07:57 +0000 2019",
 		"id": 1107086133471510500,
 		"id_str": "1107086133471510528",
