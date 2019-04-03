19907fe3143fd3c788154d8ca4bf3e30db58cf7d
kulkul <e_bowis@yahoo.com>
2019-04-03 09:14:53 +0800

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8810,
-	"created_at": "Sun Mar 31 01:14:52 +0000 2019",
+	"tweets": 8813,
+	"created_at": "Wed Apr 03 01:14:52 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,532 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Wed Apr 03 00:49:00 +0000 2019",
+		"id": 1113241958162423800,
+		"id_str": "1113241958162423808",
+		"full_text": "Hukuman di India buat anak yg lupa bikin P.R https://t.co/18V4CdRXF8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1113241947081019400,
+					"id_str": "1113241947081019393",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D3MHpxTUEAEl9SR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3MHpxTUEAEl9SR.jpg",
+					"url": "https://t.co/18V4CdRXF8",
+					"display_url": "pic.twitter.com/18V4CdRXF8",
+					"expanded_url": "https://twitter.com/putuebo/status/1113241958162423808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1113241947081019400,
+					"id_str": "1113241947081019393",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D3MHpxTUEAEl9SR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3MHpxTUEAEl9SR.jpg",
+					"url": "https://t.co/18V4CdRXF8",
+					"display_url": "pic.twitter.com/18V4CdRXF8",
+					"expanded_url": "https://twitter.com/putuebo/status/1113241958162423808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 619,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1428,
+			"friends_count": 946,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 8003,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8727,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "cf712fd91280ebff",
+			"url": "https://api.twitter.com/1.1/geo/id/cf712fd91280ebff.json",
+			"place_type": "city",
+			"name": "Denpasar Barat",
+			"full_name": "Denpasar Barat, Indonesia",
+			"country_code": "ID",
+			"country": "Indonesia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							115.173695,
+							-8.70934
+						],
+						[
+							115.235563,
+							-8.70934
+						],
+						[
+							115.235563,
+							-8.591728
+						],
+						[
+							115.173695,
+							-8.591728
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Hukuman di India buat anak yg lupa bikin P.R https://t.co/18V4CdRXF8"
+	},
+	{
+		"created_at": "Tue Apr 02 23:17:22 +0000 2019",
+		"id": 1112385011188039700,
+		"id_str": "1113218896025096194",
+		"full_text": "Meme rant thread. Please, do enjoy yourself. https://t.co/N9RIQTkoxh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112385004137373700,
+					"id_str": "1112385004137373696",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D2_8RIEUYAAnWuK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2_8RIEUYAAnWuK.jpg",
+					"url": "https://t.co/N9RIQTkoxh",
+					"display_url": "pic.twitter.com/N9RIQTkoxh",
+					"expanded_url": "https://twitter.com/momonban/status/1112385011188039681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 763,
+							"h": 767,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 763,
+							"h": 767,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112385004137373700,
+					"id_str": "1112385004137373696",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D2_8RIEUYAAnWuK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2_8RIEUYAAnWuK.jpg",
+					"url": "https://t.co/N9RIQTkoxh",
+					"display_url": "pic.twitter.com/N9RIQTkoxh",
+					"expanded_url": "https://twitter.com/momonban/status/1112385011188039681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 763,
+							"h": 767,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 763,
+							"h": 767,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1083408957228539900,
+			"id_str": "1083408957228539904",
+			"name": "Momon .",
+			"screen_name": "momonban",
+			"location": "",
+			"description": "M27 . Alter . NSFW . Movie Buff",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 419,
+			"friends_count": 666,
+			"listed_count": 0,
+			"created_at": "Thu Jan 10 17:03:18 +0000 2019",
+			"favourites_count": 2275,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6383,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1109621183782088706/lEjkf6D2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1109621183782088706/lEjkf6D2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1083408957228539904/1547820635",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2902,
+		"favorite_count": 3980,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Meme rant thread. Please, do enjoy yourself. https://t.co/N9RIQTkoxh"
+	},
+	{
+		"created_at": "Tue Apr 02 22:55:09 +0000 2019",
+		"id": 1112864888378994700,
+		"id_str": "1113213307765137408",
+		"full_text": "Bagaimana perusahaan/instansi mengelola akun media sosial? https://t.co/Sug6f38mef",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112864875137679400,
+					"id_str": "1112864875137679360",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/D3GwtS2XQAA28nY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3GwtS2XQAA28nY.jpg",
+					"url": "https://t.co/Sug6f38mef",
+					"display_url": "pic.twitter.com/Sug6f38mef",
+					"expanded_url": "https://twitter.com/ndorokakung/status/1112864888378994689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 581,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1404,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1026,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112864875137679400,
+					"id_str": "1112864875137679360",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/D3GwtS2XQAA28nY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3GwtS2XQAA28nY.jpg",
+					"url": "https://t.co/Sug6f38mef",
+					"display_url": "pic.twitter.com/Sug6f38mef",
+					"expanded_url": "https://twitter.com/ndorokakung/status/1112864888378994689/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 581,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1404,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1026,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18598424,
+			"id_str": "18598424",
+			"name": "Wicaksono 🇮🇩",
+			"screen_name": "ndorokakung",
+			"location": "Jakarta Capital Region",
+			"description": "Digital curator | try to help you find the people worth listening to and rescue their stories from an ocean of noise.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 249757,
+			"friends_count": 582,
+			"listed_count": 1383,
+			"created_at": "Sun Jan 04 07:53:57 +0000 2009",
+			"favourites_count": 1669,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 194864,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/631464170404184066/kN9h7OmD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/631464170404184066/kN9h7OmD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18598424/1465963048",
+			"profile_link_color": "381AEA",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E8E3E3",
+			"profile_text_color": "0C3E53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 123,
+		"favorite_count": 422,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Bagaimana perusahaan/instansi mengelola akun media sosial? https://t.co/Sug6f38mef"
+	}
+]
