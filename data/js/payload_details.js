11800d274fa6bba88d3033d3708d37e4799ce404
kulkul <e_bowis@yahoo.com>
2019-09-17 09:14:52 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9169,
-	"created_at": "Tue Sep 10 01:14:51 +0000 2019",
+	"tweets": 9171,
+	"created_at": "Tue Sep 17 01:14:51 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,391 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 16 10:28:20 +0000 2019",
+		"id": 1173544139427725300,
+		"id_str": "1173544139427725312",
+		"full_text": "Jaga kesehatan, Berpikir sederhana, yang penting \"ukup\"",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1442,
+			"friends_count": 969,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 10179,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9139,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "in",
+		"text": "Jaga kesehatan, Berpikir sederhana, yang penting \"ukup\""
+	},
+	{
+		"created_at": "Mon Sep 16 10:26:03 +0000 2019",
+		"id": 1173543563604291600,
+		"id_str": "1173543563604291584",
+		"full_text": "Jika berkunjung ke museum 3D I Am Bali di Monumen Bajra Sandhi Renon.. dapat anda nikmati seni yg terpajang adalah karya2 almarhum I Ketut Arsadana (dan team) #RIP - Foto nemu di google https://t.co/28fW4U8b0M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RIP",
+					"indices": [
+						159,
+						163
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1173543553839943700,
+					"id_str": "1173543553839943680",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EElDpP8UUAANlLp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EElDpP8UUAANlLp.jpg",
+					"url": "https://t.co/28fW4U8b0M",
+					"display_url": "pic.twitter.com/28fW4U8b0M",
+					"expanded_url": "https://twitter.com/putuebo/status/1173543563604291584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1173543553839943700,
+					"id_str": "1173543553839943680",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EElDpP8UUAANlLp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EElDpP8UUAANlLp.jpg",
+					"url": "https://t.co/28fW4U8b0M",
+					"display_url": "pic.twitter.com/28fW4U8b0M",
+					"expanded_url": "https://twitter.com/putuebo/status/1173543563604291584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1173543553823207400,
+					"id_str": "1173543553823207425",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EElDpP4U8AEdaXd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EElDpP4U8AEdaXd.jpg",
+					"url": "https://t.co/28fW4U8b0M",
+					"display_url": "pic.twitter.com/28fW4U8b0M",
+					"expanded_url": "https://twitter.com/putuebo/status/1173543563604291584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 719,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 719,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1173543553835757600,
+					"id_str": "1173543553835757568",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EElDpP7UcAAq8Y8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EElDpP7UcAAq8Y8.jpg",
+					"url": "https://t.co/28fW4U8b0M",
+					"display_url": "pic.twitter.com/28fW4U8b0M",
+					"expanded_url": "https://twitter.com/putuebo/status/1173543563604291584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 499,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 792,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 792,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1173543554141999000,
+					"id_str": "1173543554141999104",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/EElDpREVUAAzwDU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EElDpREVUAAzwDU.jpg",
+					"url": "https://t.co/28fW4U8b0M",
+					"display_url": "pic.twitter.com/28fW4U8b0M",
+					"expanded_url": "https://twitter.com/putuebo/status/1173543563604291584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 570,
+							"h": 300,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 570,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 570,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1173507825072361500,
+		"in_reply_to_status_id_str": "1173507825072361472",
+		"in_reply_to_user_id": 14065297,
+		"in_reply_to_user_id_str": "14065297",
+		"in_reply_to_screen_name": "putuebo",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1442,
+			"friends_count": 969,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 10179,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9139,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Jika berkunjung ke museum 3D I Am Bali di Monumen Bajra Sandhi Renon.. dapat anda nikmati seni yg terpajang adalah karya2 almarhum I Ketut Arsadana (dan team) #RIP - Foto nemu di google https://t.co/28fW4U8b0M"
+	},
+	{
 		"created_at": "Mon Sep 09 05:10:41 +0000 2019",
 		"id": 1170927484482646000,
 		"id_str": "1170927484482646018",
