6a8554ae460960953178e082043dfc8ddd13fb7a
kulkul <e_bowis@yahoo.com>
2019-11-02 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9184,
-	"created_at": "Thu Oct 31 01:14:51 +0000 2019",
+	"tweets": 9185,
+	"created_at": "Sat Nov 02 01:14:51 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 03:28:59 +0000 2019",
+		"id": 1190057667567349800,
+		"id_str": "1190108447913566211",
+		"full_text": "@WagimanDeep \"Halo, radio Boz? Minta lagunya Queen, judulnya Aibon!\"\nPenyiar:\n\"mana ada??\"\n\"Ada, - Aibon to break free...\"",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			122
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WagimanDeep",
+					"name": "Wagiman Deep (Habeib Selow)",
+					"id": 1141783903465701400,
+					"id_str": "1141783903465701376",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1189883960093921300,
+		"in_reply_to_status_id_str": "1189883960093921282",
+		"in_reply_to_user_id": 1141783903465701400,
+		"in_reply_to_user_id_str": "1141783903465701376",
+		"in_reply_to_screen_name": "WagimanDeep",
+		"user": {
+			"id": 97837665,
+			"id_str": "97837665",
+			"name": "Farah R Javaid",
+			"screen_name": "HeyFarahhhh",
+			"location": "",
+			"description": "Just an ordinary woman with extraordinary lovely life",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1059,
+			"friends_count": 247,
+			"listed_count": 1,
+			"created_at": "Sat Dec 19 06:17:17 +0000 2009",
+			"favourites_count": 4451,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4095,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1173807555098730497/NSEY08qD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1173807555098730497/NSEY08qD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/97837665/1556184586",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "in",
+		"text": "@WagimanDeep \"Halo, radio Boz? Minta lagunya Queen, judulnya Aibon!\"\nPenyiar:\n\"mana ada??\"\n\"Ada, - Aibon to break free...\""
+	}
+]
