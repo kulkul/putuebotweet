69fff4f3db88bfa07e9bb18116ba5b6a035546a4
kulkul <e_bowis@yahoo.com>
2020-02-02 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9426,
-	"created_at": "Wed Jan 29 01:14:54 +0000 2020",
+	"tweets": 9427,
+	"created_at": "Sun Feb 02 01:14:52 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,207 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sun Feb 02 00:23:31 +0000 2020",
+		"id": 1223527003195707400,
+		"id_str": "1223763846013173760",
+		"full_text": "Iklan lord didi kempot bikinan dimas djay ini keren lah...ambyarrr https://t.co/SUllzUGE4z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1223526914817581000,
+					"id_str": "1223526914817581056",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1223526914817581056/pu/img/ljJRMkPyI_8QWkrU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1223526914817581056/pu/img/ljJRMkPyI_8QWkrU.jpg",
+					"url": "https://t.co/SUllzUGE4z",
+					"display_url": "pic.twitter.com/SUllzUGE4z",
+					"expanded_url": "https://twitter.com/danangcatur/status/1223527003195707392/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223526914817581000,
+					"id_str": "1223526914817581056",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1223526914817581056/pu/img/ljJRMkPyI_8QWkrU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1223526914817581056/pu/img/ljJRMkPyI_8QWkrU.jpg",
+					"url": "https://t.co/SUllzUGE4z",
+					"display_url": "pic.twitter.com/SUllzUGE4z",
+					"expanded_url": "https://twitter.com/danangcatur/status/1223527003195707392/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 78000,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1223526914817581056/pu/vid/480x270/_1WMpoktSwRtipDI.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1223526914817581056/pu/pl/YiXn28WWuQ38_zVf.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1223526914817581056/pu/vid/640x360/u2WFH31YkcLFOWOj.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 54942666,
+			"id_str": "54942666",
+			"name": "Aku dan Kau",
+			"screen_name": "danangcatur",
+			"location": "Yogyakarta",
+			"description": "pengguna paket data & internet,\nkalo aktif di twitter berarti lagi gak ada kerjaan...\n5129427911",
+			"url": "https://t.co/6gZq3zRWPW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6gZq3zRWPW",
+							"expanded_url": "https://www.instagram.com/caturdanang/",
+							"display_url": "instagram.com/caturdanang/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 518,
+			"friends_count": 1450,
+			"listed_count": 3,
+			"created_at": "Wed Jul 08 16:18:32 +0000 2009",
+			"favourites_count": 38493,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8435,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152986383784349696/4QUvGBdz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152986383784349696/4QUvGBdz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/54942666/1564649855",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1417,
+		"favorite_count": 2180,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Iklan lord didi kempot bikinan dimas djay ini keren lah...ambyarrr https://t.co/SUllzUGE4z"
+	}
+]
