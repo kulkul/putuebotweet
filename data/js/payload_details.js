00e439810897ba4933ce6b526a3563573edb0544
kulkul <e_bowis@yahoo.com>
2020-03-06 09:14:53 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9504,
-	"created_at": "Thu Mar 05 01:14:52 +0000 2020",
+	"tweets": 9506,
+	"created_at": "Fri Mar 06 01:14:52 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 05 23:46:11 +0000 2020",
+		"id": 1235713247623254000,
+		"id_str": "1235713247623254021",
+		"full_text": "Barangkali ini salah satu penyebab corona susah masuk Indonesia, justru karena cara hidup nggak sehat kebanyakan org Indonesia. Ternyata virus ini takut sama rokok.\n\n“WHO : Merokok, Salah satu solusi pencegahan Covid-19.” https://t.co/kuGPbVqX70",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kuGPbVqX70",
+					"expanded_url": "https://link.medium.com/6ncTF1EtC4",
+					"display_url": "link.medium.com/6ncTF1EtC4",
+					"indices": [
+						222,
+						245
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2450,
+			"friends_count": 2111,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 11693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9513,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Barangkali ini salah satu penyebab corona susah masuk Indonesia, justru karena cara hidup nggak sehat kebanyakan org Indonesia. Ternyata virus ini takut sama rokok.\n\n“WHO : Merokok, Salah satu solusi pencegahan Covid-19.” https://t.co/kuGPbVqX70"
+	},
+	{
+		"created_at": "Thu Mar 05 17:04:37 +0000 2020",
+		"id": 1235612191077707800,
+		"id_str": "1235612191077707777",
+		"full_text": "@ephraym8 @TheMossadIL Namanya gambar autostereogram. Susah ngajarinnya.. intinya: titik fokus pandangam bukan di gambar tapi memandang jauh seakan2 obyek jauh.. gambar kalau gak fokus bakal jadi blur. Justru saat gambar blur akan membentuk obyek 3d. Btw I ♥️ u too ^_^",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ephraym8",
+					"name": "𝐞𝐩𝐡𝐫𝐚𝐲𝐦 ✨",
+					"id": 220215415,
+					"id_str": "220215415",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "TheMossadIL",
+					"name": "The Mossad: Elite Parody Division",
+					"id": 774174489458331600,
+					"id_str": "774174489458331649",
+					"indices": [
+						10,
+						22
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1235551019037163500,
+		"in_reply_to_status_id_str": "1235551019037163520",
+		"in_reply_to_user_id": 220215415,
+		"in_reply_to_user_id_str": "220215415",
+		"in_reply_to_screen_name": "ephraym8",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2450,
+			"friends_count": 2111,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 11693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9513,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "in",
+		"text": "@ephraym8 @TheMossadIL Namanya gambar autostereogram. Susah ngajarinnya.. intinya: titik fokus pandangam bukan di gambar tapi memandang jauh seakan2 obyek jauh.. gambar kalau gak fokus bakal jadi blur. Justru saat gambar blur akan membentuk obyek 3d. Btw I ♥️ u too ^_^"
+	},
+	{
 		"created_at": "Wed Mar 04 16:40:08 +0000 2020",
 		"id": 1235243640810868700,
 		"id_str": "1235243640810868737",
