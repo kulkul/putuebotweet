3317d543fe304279ce1b1cb4f50635dfaab86b54
kulkul <e_bowis@yahoo.com>
2020-04-02 09:14:54 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9544,
-	"created_at": "Tue Mar 31 01:14:52 +0000 2020",
+	"tweets": 9545,
+	"created_at": "Thu Apr 02 01:14:53 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 18:18:32 +0000 2020",
+		"id": 1245415264797237200,
+		"id_str": "1245415264797237255",
+		"full_text": "Cctv galat https://t.co/Il6RWr5Fp9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245415198514544600,
+					"id_str": "1245415198514544641",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245415198514544641/pu/img/qeMtP2JFN17hEaNE.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245415198514544641/pu/img/qeMtP2JFN17hEaNE.jpg",
+					"url": "https://t.co/Il6RWr5Fp9",
+					"display_url": "pic.twitter.com/Il6RWr5Fp9",
+					"expanded_url": "https://twitter.com/putuebo/status/1245415264797237255/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245415198514544600,
+					"id_str": "1245415198514544641",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1245415198514544641/pu/img/qeMtP2JFN17hEaNE.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1245415198514544641/pu/img/qeMtP2JFN17hEaNE.jpg",
+					"url": "https://t.co/Il6RWr5Fp9",
+					"display_url": "pic.twitter.com/Il6RWr5Fp9",
+					"expanded_url": "https://twitter.com/putuebo/status/1245415264797237255/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 332,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							83,
+							120
+						],
+						"duration_millis": 10566,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245415198514544641/pu/vid/332x480/68drCNikJa4FsLVC.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1245415198514544641/pu/vid/320x462/eeO4cixkpCvjOmhu.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1245415198514544641/pu/pl/csy41j-biE5F3RBs.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2408,
+			"friends_count": 2103,
+			"listed_count": 15,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 11878,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9562,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Cctv galat https://t.co/Il6RWr5Fp9"
+	}
+]
