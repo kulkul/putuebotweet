0d383709a3c6b35f7629cbe4eaaa332fe31f5c2a
kulkul <e_bowis@yahoo.com>
2020-04-14 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9578,
-	"created_at": "Sun Apr 12 01:14:52 +0000 2020",
+	"tweets": 9579,
+	"created_at": "Tue Apr 14 01:14:52 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 13 08:55:34 +0000 2020",
+		"id": 1249622244881395700,
+		"id_str": "1249622244881395721",
+		"full_text": "@PenguinSharonMy @GetVideoBot Sini saya bantu mbak, kalau getvideobot gak berfungsi unduh aja videonya via link ini: https://t.co/ySZfL2gtcl",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PenguinSharonMy",
+					"name": "Penguin",
+					"id": 1135419005055823900,
+					"id_str": "1135419005055823873",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "GetVideoBot",
+					"name": "GetVideoBot",
+					"id": 708564331332444200,
+					"id_str": "708564331332444160",
+					"indices": [
+						17,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ySZfL2gtcl",
+					"expanded_url": "https://mega.nz/file/LhwRCSrY#J_MGoeeJ1UeIyPIYUX4Xc5v3HE-sHPvIk3yk7qxszT0",
+					"display_url": "mega.nz/file/LhwRCSrY#…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1249584800731508700,
+		"in_reply_to_status_id_str": "1249584800731508737",
+		"in_reply_to_user_id": 1135419005055823900,
+		"in_reply_to_user_id_str": "1135419005055823873",
+		"in_reply_to_screen_name": "PenguinSharonMy",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2417,
+			"friends_count": 2094,
+			"listed_count": 14,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12004,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9600,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "@PenguinSharonMy @GetVideoBot Sini saya bantu mbak, kalau getvideobot gak berfungsi unduh aja videonya via link ini: https://t.co/ySZfL2gtcl"
+	},
+	{
 		"created_at": "Sat Apr 11 13:19:50 +0000 2020",
 		"id": 1248963971433894000,
 		"id_str": "1248963971433893888",
