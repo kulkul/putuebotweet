69ac63980e9b4bc5dc003fbaeeb067cb5878c94a
kulkul <e_bowis@yahoo.com>
2020-06-01 09:14:54 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9634,
-	"created_at": "Sat May 30 01:14:52 +0000 2020",
+	"tweets": 9635,
+	"created_at": "Mon Jun 01 01:14:53 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 12:35:15 +0000 2020",
+		"id": 1267072146263736300,
+		"id_str": "1267072146263736321",
+		"full_text": "@asliga Schindler's List, Life is Beautiful, 3 Idiots, Dangal https://t.co/XbDgzDarLZ",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "asliga",
+					"name": "Witra Asliga",
+					"id": 339199411,
+					"id_str": "339199411",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267072091548815400,
+					"id_str": "1267072091548815361",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWLWqGUEAETBBh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWLWqGUEAETBBh.jpg",
+					"url": "https://t.co/XbDgzDarLZ",
+					"display_url": "pic.twitter.com/XbDgzDarLZ",
+					"expanded_url": "https://twitter.com/putuebo/status/1267072146263736321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267072091548815400,
+					"id_str": "1267072091548815361",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWLWqGUEAETBBh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWLWqGUEAETBBh.jpg",
+					"url": "https://t.co/XbDgzDarLZ",
+					"display_url": "pic.twitter.com/XbDgzDarLZ",
+					"expanded_url": "https://twitter.com/putuebo/status/1267072146263736321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 451,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1267072104295305200,
+					"id_str": "1267072104295305216",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWLXZlUEAAgxFL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWLXZlUEAAgxFL.jpg",
+					"url": "https://t.co/XbDgzDarLZ",
+					"display_url": "pic.twitter.com/XbDgzDarLZ",
+					"expanded_url": "https://twitter.com/putuebo/status/1267072146263736321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1267072115917721600,
+					"id_str": "1267072115917721600",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWLYE4UEAAJY8b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWLYE4UEAAJY8b.jpg",
+					"url": "https://t.co/XbDgzDarLZ",
+					"display_url": "pic.twitter.com/XbDgzDarLZ",
+					"expanded_url": "https://twitter.com/putuebo/status/1267072146263736321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1267072136306241500,
+					"id_str": "1267072136306241536",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWLZQ1UMAAvUOO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWLZQ1UMAAvUOO.jpg",
+					"url": "https://t.co/XbDgzDarLZ",
+					"display_url": "pic.twitter.com/XbDgzDarLZ",
+					"expanded_url": "https://twitter.com/putuebo/status/1267072146263736321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 376,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 376,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 376,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1266715471862132700,
+		"in_reply_to_status_id_str": "1266715471862132736",
+		"in_reply_to_user_id": 339199411,
+		"in_reply_to_user_id_str": "339199411",
+		"in_reply_to_screen_name": "asliga",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2403,
+			"friends_count": 2063,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12093,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@asliga Schindler's List, Life is Beautiful, 3 Idiots, Dangal https://t.co/XbDgzDarLZ"
+	},
+	{
 		"created_at": "Sat May 30 00:59:40 +0000 2020",
 		"id": 1266534709141491700,
 		"id_str": "1266534709141491712",
