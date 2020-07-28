4d3c1674fc7651d0ff3594d4c6b40490161c657c
kulkul <e_bowis@yahoo.com>
2020-07-28 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9643,
-	"created_at": "Wed Jul 01 01:14:52 +0000 2020",
+	"tweets": 9644,
+	"created_at": "Tue Jul 28 01:14:51 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,258 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Mon Jul 27 02:15:55 +0000 2020",
+		"id": 1287258629297434600,
+		"id_str": "1287572394983346176",
+		"full_text": "Cool #AR powered glasses 👍\n\n#AI #VR #MR #DigitalTransformation #IOT #3Danimation https://t.co/5muFNOdYuh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AR",
+					"indices": [
+						5,
+						8
+					]
+				},
+				{
+					"text": "AI",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "VR",
+					"indices": [
+						32,
+						35
+					]
+				},
+				{
+					"text": "MR",
+					"indices": [
+						36,
+						39
+					]
+				},
+				{
+					"text": "DigitalTransformation",
+					"indices": [
+						40,
+						62
+					]
+				},
+				{
+					"text": "IOT",
+					"indices": [
+						63,
+						67
+					]
+				},
+				{
+					"text": "3Danimation",
+					"indices": [
+						68,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1287258598456746000,
+					"id_str": "1287258598456745991",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1287258598456745991/pu/img/WYxJ0DVMg4hcmjnc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1287258598456745991/pu/img/WYxJ0DVMg4hcmjnc.jpg",
+					"url": "https://t.co/5muFNOdYuh",
+					"display_url": "pic.twitter.com/5muFNOdYuh",
+					"expanded_url": "https://twitter.com/alvinfoo/status/1287258629297434624/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1287258598456746000,
+					"id_str": "1287258598456745991",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1287258598456745991/pu/img/WYxJ0DVMg4hcmjnc.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1287258598456745991/pu/img/WYxJ0DVMg4hcmjnc.jpg",
+					"url": "https://t.co/5muFNOdYuh",
+					"display_url": "pic.twitter.com/5muFNOdYuh",
+					"expanded_url": "https://twitter.com/alvinfoo/status/1287258629297434624/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 15067,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1287258598456745991/pu/vid/360x450/0l5kQinbGffUNqnJ.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1287258598456745991/pu/vid/320x400/WeIZLnO4sbeRR-wY.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1287258598456745991/pu/vid/640x800/3470ZIXA4ikQYudy.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1287258598456745991/pu/pl/r5JZATFawCgdjFqh.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 794815,
+			"id_str": "794815",
+			"name": "Alvin Foo",
+			"screen_name": "alvinfoo",
+			"location": "china",
+			"description": "#Artificialintelligence | #DigitalTransformation | #FutureofWork | #Marketing | #ElectricVehicle | #Tesla",
+			"url": "https://t.co/slct67vAjs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/slct67vAjs",
+							"expanded_url": "http://linkedin.com/in/alvinfsc",
+							"display_url": "linkedin.com/in/alvinfsc",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 52335,
+			"friends_count": 14305,
+			"listed_count": 1368,
+			"created_at": "Mon Feb 26 08:29:59 +0000 2007",
+			"favourites_count": 2007,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 57899,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/921877141389635584/ykzP1VKB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/921877141389635584/ykzP1VKB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/794815/1441416816",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2666,
+		"favorite_count": 8890,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Cool #AR powered glasses 👍\n\n#AI #VR #MR #DigitalTransformation #IOT #3Danimation https://t.co/5muFNOdYuh"
+	}
+]
