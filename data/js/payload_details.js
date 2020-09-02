d9a818a838ab4bdb474e24d3ae5fdb229e80be2f
kulkul <e_bowis@yahoo.com>
2020-09-02 09:14:55 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9646,
-	"created_at": "Thu Aug 27 01:14:53 +0000 2020",
+	"tweets": 9647,
+	"created_at": "Wed Sep 02 01:14:53 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_09.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2020_09 = 
+[
+	{
+		"created_at": "Tue Sep 01 05:07:14 +0000 2020",
+		"id": 1300661233708753000,
+		"id_str": "1300661470024220674",
+		"full_text": "Nanti sore opening. Pameran Kartun Denpasar. \"Hindu di mata kartunis Bali\". Di Galeri Bukuku, jl. Nangka Selatan 29a Denpasar. Pameran berlangsung sampai 1 Oktober 2020 https://t.co/3vN5W8FVZb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1300661230382715000,
+					"id_str": "1300661230382714881",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/Egzgf3IVkAEqY7m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Egzgf3IVkAEqY7m.jpg",
+					"url": "https://t.co/3vN5W8FVZb",
+					"display_url": "pic.twitter.com/3vN5W8FVZb",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1300661233708752896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 874,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1300661230382715000,
+					"id_str": "1300661230382714881",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/Egzgf3IVkAEqY7m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Egzgf3IVkAEqY7m.jpg",
+					"url": "https://t.co/3vN5W8FVZb",
+					"display_url": "pic.twitter.com/3vN5W8FVZb",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1300661233708752896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 874,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1197426280796283000,
+			"id_str": "1197426280796282882",
+			"name": "Peh! Bali Cartoon",
+			"screen_name": "PehBaliCartoon",
+			"location": "Badung - Bali",
+			"description": "The the the the the Bali Cartoon",
+			"url": "https://t.co/TfrXy312je",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TfrXy312je",
+							"expanded_url": "https://pehbalicartoon.com",
+							"display_url": "pehbalicartoon.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12,
+			"friends_count": 65,
+			"listed_count": 0,
+			"created_at": "Thu Nov 21 08:07:38 +0000 2019",
+			"favourites_count": 106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 45,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1279629100596948992/aGz7pKVy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1279629100596948992/aGz7pKVy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1197426280796282882/1586787787",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Nanti sore opening. Pameran Kartun Denpasar. \"Hindu di mata kartunis Bali\". Di Galeri Bukuku, jl. Nangka Selatan 29a Denpasar. Pameran berlangsung sampai 1 Oktober 2020 https://t.co/3vN5W8FVZb"
+	}
+]
