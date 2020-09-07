8d37eaef2ee59df00e34750ffbd02823975d09df
kulkul <e_bowis@yahoo.com>
2020-09-07 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9647,
-	"created_at": "Wed Sep 02 01:14:53 +0000 2020",
+	"tweets": 9648,
+	"created_at": "Mon Sep 07 01:14:52 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sun Sep 06 09:38:27 +0000 2020",
+		"id": 1300742732688683000,
+		"id_str": "1302541664074887168",
+		"full_text": "Actually inilah yg kulihat sehari2 di otakku... #supportsenimanlokal😁 https://t.co/msYt3Aw7bp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "supportsenimanlokal",
+					"indices": [
+						48,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/msYt3Aw7bp",
+					"expanded_url": "https://twitter.com/alpinnn/status/1300668246987694086",
+					"display_url": "twitter.com/alpinnn/status…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1197023131669418000,
+			"id_str": "1197023131669417986",
+			"name": "Gus Dark",
+			"screen_name": "GusDark4",
+			"location": "Bali",
+			"description": "an Illustrator, propaganda, activist, creative, most of all Human beings, a Balinese from small island of paradise. IG : gus_dark_art.",
+			"url": "https://t.co/oQa5vuWyjW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oQa5vuWyjW",
+							"expanded_url": "http://gusthebalinese.wixsite.com/darkade",
+							"display_url": "gusthebalinese.wixsite.com/darkade",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 430,
+			"friends_count": 473,
+			"listed_count": 1,
+			"created_at": "Wed Nov 20 05:25:35 +0000 2019",
+			"favourites_count": 9423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3033,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1197026444267835393/T5Qjr5rf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1197026444267835393/T5Qjr5rf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1197023131669417986/1586179650",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1300668246987694000,
+		"quoted_status_id_str": "1300668246987694086",
+		"quoted_status_permalink": {
+			"url": "https://t.co/msYt3Aw7bp",
+			"expanded": "https://twitter.com/alpinnn/status/1300668246987694086",
+			"display": "twitter.com/alpinnn/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 01 05:34:09 +0000 2020",
+			"id": 1300668246987694000,
+			"id_str": "1300668246987694086",
+			"full_text": "Iklan sotosop ini sungguh 😱😱😱 https://t.co/vrrqKencD0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				29
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1300668145309347800,
+						"id_str": "1300668145309347840",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1300668145309347840/pu/img/CB4waiqUt7uJuBe5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1300668145309347840/pu/img/CB4waiqUt7uJuBe5.jpg",
+						"url": "https://t.co/vrrqKencD0",
+						"display_url": "pic.twitter.com/vrrqKencD0",
+						"expanded_url": "https://twitter.com/alpinnn/status/1300668246987694086/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1300668145309347800,
+						"id_str": "1300668145309347840",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1300668145309347840/pu/img/CB4waiqUt7uJuBe5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1300668145309347840/pu/img/CB4waiqUt7uJuBe5.jpg",
+						"url": "https://t.co/vrrqKencD0",
+						"display_url": "pic.twitter.com/vrrqKencD0",
+						"expanded_url": "https://twitter.com/alpinnn/status/1300668246987694086/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 60059,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1300668145309347840/pu/vid/480x270/W0XNqMMHPxT7XV4I.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1300668145309347840/pu/pl/ZaNVYfIJR2OKAt3j.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1300668145309347840/pu/vid/640x360/T6be3UY3jODI615r.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1300668145309347840/pu/vid/1280x720/VOQuDvOko7LkSuLp.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 217731563,
+				"id_str": "217731563",
+				"name": "Alpine J Pribadhy",
+				"screen_name": "alpinnn",
+				"location": "",
+				"description": "Hanya sebuah akun yang engagementnya kecil.",
+				"url": "https://t.co/VVLgzqEEUB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VVLgzqEEUB",
+								"expanded_url": "https://www.instagram.com/tinggal.bareng",
+								"display_url": "instagram.com/tinggal.bareng",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5922,
+				"friends_count": 972,
+				"listed_count": 18,
+				"created_at": "Sat Nov 20 10:58:08 +0000 2010",
+				"favourites_count": 364,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 56720,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1248534516945174528/uG8pXo1a_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1248534516945174528/uG8pXo1a_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/217731563/1397166377",
+				"profile_link_color": "FAB81E",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 23689,
+			"favorite_count": 65850,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "in"
+		},
+		"retweet_count": 5,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Actually inilah yg kulihat sehari2 di otakku... #supportsenimanlokal😁 https://t.co/msYt3Aw7bp"
+	},
+	{
 		"created_at": "Tue Sep 01 05:07:14 +0000 2020",
 		"id": 1300661233708753000,
 		"id_str": "1300661470024220674",
