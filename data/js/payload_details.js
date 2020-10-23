a45c211a96d5bf9609071096dde6db3cb6c71d35
kulkul <e_bowis@yahoo.com>
2020-10-23 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9669,
-	"created_at": "Tue Oct 20 01:14:51 +0000 2020",
+	"tweets": 9670,
+	"created_at": "Fri Oct 23 01:14:52 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 22 16:03:35 +0000 2020",
+		"id": 1319188327396831200,
+		"id_str": "1319308426170626048",
+		"full_text": "The #Quidditch World Cup 2020 tertunda gara gara pandemi. Padahal tim asosiasi sekolah sihir Bali udah siap nok.. 😆 https://t.co/xLB14LnObV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Quidditch",
+					"indices": [
+						4,
+						14
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1319188313769537500,
+					"id_str": "1319188313769537537",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek6yyDIUcAEcbp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek6yyDIUcAEcbp6.jpg",
+					"url": "https://t.co/xLB14LnObV",
+					"display_url": "pic.twitter.com/xLB14LnObV",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1319188327396831235/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1319188313769537500,
+					"id_str": "1319188313769537537",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek6yyDIUcAEcbp6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek6yyDIUcAEcbp6.jpg",
+					"url": "https://t.co/xLB14LnObV",
+					"display_url": "pic.twitter.com/xLB14LnObV",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1319188327396831235/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1197426280796283000,
+			"id_str": "1197426280796282882",
+			"name": "Peh! Bali Cartoon",
+			"screen_name": "PehBaliCartoon",
+			"location": "Badung - Bali",
+			"description": "Peh Bali Cartoon Community.",
+			"url": "https://t.co/TfrXy312je",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TfrXy312je",
+							"expanded_url": "https://pehbalicartoon.com",
+							"display_url": "pehbalicartoon.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20,
+			"friends_count": 100,
+			"listed_count": 0,
+			"created_at": "Thu Nov 21 08:07:38 +0000 2019",
+			"favourites_count": 192,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 121,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1305791418477109249/wxk_QGC1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1305791418477109249/wxk_QGC1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1197426280796282882/1586787787",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "The #Quidditch World Cup 2020 tertunda gara gara pandemi. Padahal tim asosiasi sekolah sihir Bali udah siap nok.. 😆 https://t.co/xLB14LnObV"
+	},
+	{
 		"created_at": "Mon Oct 19 23:20:44 +0000 2020",
 		"id": 1318331274969239600,
 		"id_str": "1318331274969239553",
