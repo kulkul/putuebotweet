e247404ba5dae8d976c627c2ee0448b0de7cd0ba
kulkul <e_bowis@yahoo.com>
2020-11-02 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9670,
-	"created_at": "Fri Oct 23 01:14:52 +0000 2020",
+	"tweets": 9671,
+	"created_at": "Mon Nov 02 01:14:51 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 01 16:32:54 +0000 2020",
+		"id": 1322415076368765000,
+		"id_str": "1322939684108996609",
+		"full_text": "An untold scene from a Wayang Story. By @putuebo https://t.co/uR1nL4onUQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "putuebo",
+					"name": "Putu Ebo",
+					"id": 14065297,
+					"id_str": "14065297",
+					"indices": [
+						40,
+						48
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1322415066663116800,
+					"id_str": "1322415066663116806",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/ElopfyYUYAYdPFw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElopfyYUYAYdPFw.jpg",
+					"url": "https://t.co/uR1nL4onUQ",
+					"display_url": "pic.twitter.com/uR1nL4onUQ",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1322415076368764928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1322415066663116800,
+					"id_str": "1322415066663116806",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/ElopfyYUYAYdPFw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElopfyYUYAYdPFw.jpg",
+					"url": "https://t.co/uR1nL4onUQ",
+					"display_url": "pic.twitter.com/uR1nL4onUQ",
+					"expanded_url": "https://twitter.com/PehBaliCartoon/status/1322415076368764928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1197426280796283000,
+			"id_str": "1197426280796282882",
+			"name": "Peh! Bali Cartoon",
+			"screen_name": "PehBaliCartoon",
+			"location": "Badung - Bali",
+			"description": "Peh Bali Cartoon Community.",
+			"url": "https://t.co/TfrXy312je",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TfrXy312je",
+							"expanded_url": "https://pehbalicartoon.com",
+							"display_url": "pehbalicartoon.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 26,
+			"friends_count": 105,
+			"listed_count": 0,
+			"created_at": "Thu Nov 21 08:07:38 +0000 2019",
+			"favourites_count": 202,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 132,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1305791418477109249/wxk_QGC1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1305791418477109249/wxk_QGC1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1197426280796282882/1586787787",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "An untold scene from a Wayang Story. By @putuebo https://t.co/uR1nL4onUQ"
+	}
+]
