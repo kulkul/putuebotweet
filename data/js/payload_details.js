845337619bca261f7a239b1c8f4ee71169eb486d
kulkul <e_bowis@yahoo.com>
2021-01-15 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9702,
-	"created_at": "Sat Jan 09 01:14:52 +0000 2021",
+	"tweets": 9703,
+	"created_at": "Fri Jan 15 01:14:52 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Thu Jan 14 04:26:25 +0000 2021",
+		"id": 1349573561137021000,
+		"id_str": "1349573561137020930",
+		"full_text": "Belum nemu review/ulasan panjang ttg komik ini; Thn 2020, Kolaborasi karya 11 komikus Belanda menerbitkan: Storm Hommages - Lockdown Series, sbg sebuah salut utk Don Lawrence. Who is Storm? Remaja gaul 80an pasti tau... https://t.co/1BO7BqYNLF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1349573551976640500,
+					"id_str": "1349573551976640513",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/Erql_sPUcAEyTT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Erql_sPUcAEyTT3.jpg",
+					"url": "https://t.co/1BO7BqYNLF",
+					"display_url": "pic.twitter.com/1BO7BqYNLF",
+					"expanded_url": "https://twitter.com/putuebo/status/1349573561137020930/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 924,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 924,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1349573551976640500,
+					"id_str": "1349573551976640513",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/Erql_sPUcAEyTT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Erql_sPUcAEyTT3.jpg",
+					"url": "https://t.co/1BO7BqYNLF",
+					"display_url": "pic.twitter.com/1BO7BqYNLF",
+					"expanded_url": "https://twitter.com/putuebo/status/1349573561137020930/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 924,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 924,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2372,
+			"friends_count": 2016,
+			"listed_count": 14,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12220,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9737,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Belum nemu review/ulasan panjang ttg komik ini; Thn 2020, Kolaborasi karya 11 komikus Belanda menerbitkan: Storm Hommages - Lockdown Series, sbg sebuah salut utk Don Lawrence. Who is Storm? Remaja gaul 80an pasti tau... https://t.co/1BO7BqYNLF"
+	},
+	{
 		"created_at": "Fri Jan 08 16:51:37 +0000 2021",
 		"id": 1347536826630766600,
 		"id_str": "1347586771639406599",
