8c485453ec929855ae4d4526eb433d8321935cc6
kulkul <e_bowis@yahoo.com>
2021-03-14 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9728,
-	"created_at": "Thu Mar 04 01:14:50 +0000 2021",
+	"tweets": 9729,
+	"created_at": "Sun Mar 14 01:14:51 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Sat Mar 13 07:45:54 +0000 2021",
+		"id": 1370642257221546000,
+		"id_str": "1370642257221545987",
+		"full_text": "Siap-siap besok Hari Nyepi.. https://t.co/lPie3mQxx3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1370642248480649200,
+					"id_str": "1370642248480649216",
+					"indices": [
+						29,
+						52
+					],
+					"media_url": "http://pbs.twimg.com/media/EwV_3O7U8AAwYgd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwV_3O7U8AAwYgd.jpg",
+					"url": "https://t.co/lPie3mQxx3",
+					"display_url": "pic.twitter.com/lPie3mQxx3",
+					"expanded_url": "https://twitter.com/putuebo/status/1370642257221545987/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 626,
+							"h": 1111,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 626,
+							"h": 1111,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1370642248480649200,
+					"id_str": "1370642248480649216",
+					"indices": [
+						29,
+						52
+					],
+					"media_url": "http://pbs.twimg.com/media/EwV_3O7U8AAwYgd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwV_3O7U8AAwYgd.jpg",
+					"url": "https://t.co/lPie3mQxx3",
+					"display_url": "pic.twitter.com/lPie3mQxx3",
+					"expanded_url": "https://twitter.com/putuebo/status/1370642257221545987/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 626,
+							"h": 1111,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 626,
+							"h": 1111,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2350,
+			"friends_count": 1984,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12247,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9765,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Siap-siap besok Hari Nyepi.. https://t.co/lPie3mQxx3"
+	},
+	{
 		"created_at": "Wed Mar 03 15:11:17 +0000 2021",
 		"id": 1367130465686159400,
 		"id_str": "1367130465686159363",
@@ -16,7 +190,7 @@ Grailbird.data.tweets_2021_03 =
 			"user_mentions": [
 				{
 					"screen_name": "wirascorpig",
-					"name": "🇼‌🇮‌🇷‌🇦‌",
+					"name": "🇼🇮🇷🇦",
 					"id": 2868391693,
 					"id_str": "2868391693",
 					"indices": [
