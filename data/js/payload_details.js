82086d1a5eff6bd7f734632055c7107572b6e638
kulkul <e_bowis@yahoo.com>
2021-07-18 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9743,
-	"created_at": "Thu Jul 15 01:14:52 +0000 2021",
+	"tweets": 9744,
+	"created_at": "Sun Jul 18 01:14:52 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sat Jul 17 09:31:18 +0000 2021",
+		"id": 1416329656257585200,
+		"id_str": "1416329656257585152",
+		"full_text": "Hidup tidak se-anjing ini kawan.. eh, betul khan? Follow for more our IG https://t.co/ObvKEiagmn https://t.co/yfDdodKYLK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ObvKEiagmn",
+					"expanded_url": "https://www.instagram.com/theputuebo.co/",
+					"display_url": "instagram.com/theputuebo.co/",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1416328992810889200,
+					"id_str": "1416328992810889218",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E6fPtwWUcAImCzg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6fPtwWUcAImCzg.jpg",
+					"url": "https://t.co/yfDdodKYLK",
+					"display_url": "pic.twitter.com/yfDdodKYLK",
+					"expanded_url": "https://twitter.com/putuebo/status/1416329656257585152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1416328992810889200,
+					"id_str": "1416328992810889218",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E6fPtwWUcAImCzg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6fPtwWUcAImCzg.jpg",
+					"url": "https://t.co/yfDdodKYLK",
+					"display_url": "pic.twitter.com/yfDdodKYLK",
+					"expanded_url": "https://twitter.com/putuebo/status/1416329656257585152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1416329059869495300,
+					"id_str": "1416329059869495299",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E6fPxqKVkAMYx4Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6fPxqKVkAMYx4Z.jpg",
+					"url": "https://t.co/yfDdodKYLK",
+					"display_url": "pic.twitter.com/yfDdodKYLK",
+					"expanded_url": "https://twitter.com/putuebo/status/1416329656257585152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1416329108649246700,
+					"id_str": "1416329108649246721",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E6fP0f4VgAEB7Cs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6fP0f4VgAEB7Cs.jpg",
+					"url": "https://t.co/yfDdodKYLK",
+					"display_url": "pic.twitter.com/yfDdodKYLK",
+					"expanded_url": "https://twitter.com/putuebo/status/1416329656257585152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1416329179067347000,
+					"id_str": "1416329179067346946",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/E6fP4mNUcAIt8Ih.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6fP4mNUcAIt8Ih.jpg",
+					"url": "https://t.co/yfDdodKYLK",
+					"display_url": "pic.twitter.com/yfDdodKYLK",
+					"expanded_url": "https://twitter.com/putuebo/status/1416329656257585152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2326,
+			"friends_count": 1971,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12307,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9774,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Hidup tidak se-anjing ini kawan.. eh, betul khan? Follow for more our IG https://t.co/ObvKEiagmn https://t.co/yfDdodKYLK"
+	},
+	{
 		"created_at": "Wed Jul 14 19:38:45 +0000 2021",
 		"id": 1415395363024896000,
 		"id_str": "1415395363024896003",
