df340bffea0f8ab0661fd8ced66375ff005414dd
kulkul <e_bowis@yahoo.com>
2021-08-13 09:14:53 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9746,
-	"created_at": "Sat Jul 24 01:14:51 +0000 2021",
+	"tweets": 9747,
+	"created_at": "Fri Aug 13 01:14:52 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,211 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Thu Aug 12 08:48:19 +0000 2021",
+		"id": 1425631835577483300,
+		"id_str": "1425740921690230797",
+		"full_text": "kenapa bs bgni ya? yg lalu2 ngapain aja?? https://t.co/uhyowcVdWW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1425631833551642600,
+					"id_str": "1425631833551642627",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E8jcmWtVoAMKYQY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8jcmWtVoAMKYQY.jpg",
+					"url": "https://t.co/uhyowcVdWW",
+					"display_url": "pic.twitter.com/uhyowcVdWW",
+					"expanded_url": "https://twitter.com/moenthe_/status/1425631835577483268/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 552,
+							"h": 690,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 552,
+							"h": 690,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1425631833551642600,
+					"id_str": "1425631833551642627",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E8jcmWtVoAMKYQY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8jcmWtVoAMKYQY.jpg",
+					"url": "https://t.co/uhyowcVdWW",
+					"display_url": "pic.twitter.com/uhyowcVdWW",
+					"expanded_url": "https://twitter.com/moenthe_/status/1425631835577483268/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 552,
+							"h": 690,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 552,
+							"h": 690,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1279698433566109700,
+			"id_str": "1279698433566109697",
+			"name": "moenthe_59",
+			"screen_name": "moenthe_",
+			"location": "https://www.instagram.com/p/CC",
+			"description": "#NitizenProNKRI  //akun pribadi dan hanya ini\n\npengganti akun lama @mrbarens",
+			"url": "https://t.co/bUT2FQ9dbx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bUT2FQ9dbx",
+							"expanded_url": "https://www.youtube.com/channel/UCxwK0Py2cAJP91jYxi3FCSw",
+							"display_url": "youtube.com/channel/UCxwK0…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3322,
+			"friends_count": 3679,
+			"listed_count": 0,
+			"created_at": "Sun Jul 05 08:47:55 +0000 2020",
+			"favourites_count": 18043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21540,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1423239519726231554/2CnAdL6o_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1423239519726231554/2CnAdL6o_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1279698433566109697/1610297575",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "6e0f0a00ef60a2bf",
+			"url": "https://api.twitter.com/1.1/geo/id/6e0f0a00ef60a2bf.json",
+			"place_type": "admin",
+			"name": "West Borneo",
+			"full_name": "West Borneo, Indonesia",
+			"country_code": "ID",
+			"country": "Indonesia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							108.5949,
+							-3.067652
+						],
+						[
+							114.2008722,
+							-3.067652
+						],
+						[
+							114.2008722,
+							2.0810503
+						],
+						[
+							108.5949,
+							2.0810503
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1058,
+		"favorite_count": 4356,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "kenapa bs bgni ya? yg lalu2 ngapain aja?? https://t.co/uhyowcVdWW"
+	}
+]
