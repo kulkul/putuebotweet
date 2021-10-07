7abac5787ac2b78853aa4316db7a67edb13f91cc
kulkul <e_bowis@yahoo.com>
2021-10-07 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9752,
-	"created_at": "Mon Sep 27 01:15:41 +0000 2021",
+	"tweets": 9753,
+	"created_at": "Thu Oct 07 01:14:51 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_10.js
@@ -0,0 +1,202 @@
+Grailbird.data.tweets_2021_10 = 
+[
+	{
+		"created_at": "Wed Oct 06 17:26:40 +0000 2021",
+		"id": 1307381102743441400,
+		"id_str": "1445802701439201282",
+		"full_text": "@jawafess Cah cilik saiki do raiso basa jawa...:v https://t.co/it55yQSruh",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jawafess",
+					"name": "Mbud",
+					"id": 1163303067879477200,
+					"id_str": "1163303067879477248",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1307380835637575700,
+					"id_str": "1307380835637575682",
+					"indices": [
+						50,
+						73
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1307380835637575682/pu/img/DHu8W8EyePgMRJkN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1307380835637575682/pu/img/DHu8W8EyePgMRJkN.jpg",
+					"url": "https://t.co/it55yQSruh",
+					"display_url": "pic.twitter.com/it55yQSruh",
+					"expanded_url": "https://twitter.com/Agunged_Z/status/1307381102743441409/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1307380835637575700,
+					"id_str": "1307380835637575682",
+					"indices": [
+						50,
+						73
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1307380835637575682/pu/img/DHu8W8EyePgMRJkN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1307380835637575682/pu/img/DHu8W8EyePgMRJkN.jpg",
+					"url": "https://t.co/it55yQSruh",
+					"display_url": "pic.twitter.com/it55yQSruh",
+					"expanded_url": "https://twitter.com/Agunged_Z/status/1307381102743441409/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 400,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 25567,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1307380835637575682/pu/pl/aBEoyGu4qAzJKEer.m3u8?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1307380835637575682/pu/vid/320x320/6SC7QlJf9Uveefpz.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1307380835637575682/pu/vid/400x400/b-O-iNCEcHXE8FKx.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1307328655668908000,
+		"in_reply_to_status_id_str": "1307328655668908033",
+		"in_reply_to_user_id": 1163303067879477200,
+		"in_reply_to_user_id_str": "1163303067879477248",
+		"in_reply_to_screen_name": "jawafess",
+		"user": {
+			"id": 1264583065923354600,
+			"id_str": "1264583065923354625",
+			"name": "Nooblesse🍃",
+			"screen_name": "agunged_z",
+			"location": "",
+			"description": "must be nice!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2,
+			"friends_count": 25,
+			"listed_count": 0,
+			"created_at": "Sun May 24 15:44:43 +0000 2020",
+			"favourites_count": 356,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 21,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1264583399886319618/CSXuenDi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1264583399886319618/CSXuenDi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1264583065923354625/1600264351",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 68,
+		"favorite_count": 240,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "in",
+		"text": "@jawafess Cah cilik saiki do raiso basa jawa...:v https://t.co/it55yQSruh"
+	}
+]
