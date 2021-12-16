166b782d2a46f3511ed78013e12b106c6918b8b3
kulkul <e_bowis@yahoo.com>
2021-12-16 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9758,
-	"created_at": "Sun Oct 31 01:14:51 +0000 2021",
+	"tweets": 9759,
+	"created_at": "Thu Dec 16 01:14:51 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,202 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Wed Dec 15 05:06:56 +0000 2021",
+		"id": 1470423600679116800,
+		"id_str": "1470983692549910529",
+		"full_text": "@kegblgnunfaedh Lah iya... https://t.co/IVUuEpVRfk",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kegblgnunfaedh",
+					"name": "Kegoblogan.Unfaedah",
+					"id": 1172167188800655400,
+					"id_str": "1172167188800655360",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1470423277399019500,
+					"id_str": "1470423277399019522",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1470423277399019522/pu/img/Vf73C8N85nec4v4m.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1470423277399019522/pu/img/Vf73C8N85nec4v4m.jpg",
+					"url": "https://t.co/IVUuEpVRfk",
+					"display_url": "pic.twitter.com/IVUuEpVRfk",
+					"expanded_url": "https://twitter.com/almatyfibroma/status/1470423600679116807/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 460,
+							"h": 816,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 816,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1470423277399019500,
+					"id_str": "1470423277399019522",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1470423277399019522/pu/img/Vf73C8N85nec4v4m.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1470423277399019522/pu/img/Vf73C8N85nec4v4m.jpg",
+					"url": "https://t.co/IVUuEpVRfk",
+					"display_url": "pic.twitter.com/IVUuEpVRfk",
+					"expanded_url": "https://twitter.com/almatyfibroma/status/1470423600679116807/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 460,
+							"h": 816,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 460,
+							"h": 816,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							115,
+							204
+						],
+						"duration_millis": 30000,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1470423277399019522/pu/vid/460x816/nLmKqJWpAEHP7Lrx.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1470423277399019522/pu/vid/320x566/5NmX_Wg96EggltQI.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1470423277399019522/pu/pl/Ch2R_aMeRekeDR7i.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1470422420318081000,
+		"in_reply_to_status_id_str": "1470422420318081028",
+		"in_reply_to_user_id": 1172167188800655400,
+		"in_reply_to_user_id_str": "1172167188800655360",
+		"in_reply_to_screen_name": "kegblgnunfaedh",
+		"user": {
+			"id": 94998032,
+			"id_str": "94998032",
+			"name": ".tom",
+			"screen_name": "almatyfibroma",
+			"location": "Reykjavik",
+			"description": "𝓐𝓵𝓪𝓶 𝓲𝓴𝓲 𝓼𝓪𝓳𝓪𝓽𝓲𝓷𝓲𝓷𝓰 𝓰𝓾𝓻𝓾",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 598,
+			"friends_count": 665,
+			"listed_count": 3,
+			"created_at": "Sun Dec 06 13:25:39 +0000 2009",
+			"favourites_count": 1341,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10785,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BF1238",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme20/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme20/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469573838845329409/LC-Z4WbE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469573838845329409/LC-Z4WbE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94998032/1638799799",
+			"profile_link_color": "BF1238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 257,
+		"favorite_count": 1270,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "@kegblgnunfaedh Lah iya... https://t.co/IVUuEpVRfk"
+	}
+]
