166657e9eecef3256e64654c6187ac1f840f66af
kulkul <e_bowis@yahoo.com>
2022-05-06 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9774,
-	"created_at": "Mon Apr 25 01:14:51 +0000 2022",
+	"tweets": 9775,
+	"created_at": "Fri May 06 01:14:51 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Thu May 05 03:47:36 +0000 2022",
+		"id": 1522060418222821400,
+		"id_str": "1522060418222821378",
+		"full_text": "@khurram89794484 @SatoshiBtcs I just send you 0.1 tBTCs.. you can also send it back to me ...\n0xE3490BA3feD52A0fe7BF1cb91E423d852611F48c",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "khurram89794484",
+					"name": "khurram shahzad",
+					"id": 1496136332334280700,
+					"id_str": "1496136332334280704",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "SatoshiBtcs",
+					"name": "Satoshi BTCs",
+					"id": 1445421744697741300,
+					"id_str": "1445421744697741312",
+					"indices": [
+						17,
+						29
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1521826951685623800,
+		"in_reply_to_status_id_str": "1521826951685623809",
+		"in_reply_to_user_id": 1496136332334280700,
+		"in_reply_to_user_id_str": "1496136332334280704",
+		"in_reply_to_screen_name": "khurram89794484",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2289,
+			"friends_count": 1992,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12459,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9795,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@khurram89794484 @SatoshiBtcs I just send you 0.1 tBTCs.. you can also send it back to me ...\n0xE3490BA3feD52A0fe7BF1cb91E423d852611F48c"
+	}
+]
