7fd8d06916a5f90db5803508ad9a17fd968de1ce
kulkul <e_bowis@yahoo.com>
2022-05-13 09:14:51 +0800

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,325 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 12 06:56:02 +0000 2022",
+		"id": 1524644552061382700,
+		"id_str": "1524644552061382657",
+		"full_text": "@westiant @balicantik @jangopramartha \nIkutan Airdrops kuy... \nWe were born to travel to the moon https://t.co/palYkXV7BG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "westiant",
+					"name": "Windu Estianto",
+					"id": 50566435,
+					"id_str": "50566435",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "balicantik",
+					"name": "Herbi Mery",
+					"id": 104966404,
+					"id_str": "104966404",
+					"indices": [
+						10,
+						21
+					]
+				},
+				{
+					"screen_name": "jangopramartha",
+					"name": "jango pramartha",
+					"id": 281930087,
+					"id_str": "281930087",
+					"indices": [
+						22,
+						37
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/palYkXV7BG",
+					"expanded_url": "https://twitter.com/space_doge_coin/status/1518283254734127104",
+					"display_url": "twitter.com/space_doge_coi…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 50566435,
+		"in_reply_to_user_id_str": "50566435",
+		"in_reply_to_screen_name": "westiant",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2290,
+			"friends_count": 1993,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12464,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9797,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1518283254734127000,
+		"quoted_status_id_str": "1518283254734127104",
+		"quoted_status_permalink": {
+			"url": "https://t.co/palYkXV7BG",
+			"expanded": "https://twitter.com/space_doge_coin/status/1518283254734127104",
+			"display": "twitter.com/space_doge_coi…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Apr 24 17:38:30 +0000 2022",
+			"id": 1518283254734127000,
+			"id_str": "1518283254734127104",
+			"full_text": "We were born to travel to the moon with Elon Musk\n50% of tokens will be paid to Elon Musk (oblation)\nOur tokens have strong dividends\nBUY fee 10% (100% distributed to token holders)\nSELL 10~30% (differentiated by period) (100% distributed to token holders) https://t.co/bb0WtmFOnH",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				256
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1518283239089393700,
+						"id_str": "1518283239089393664",
+						"indices": [
+							257,
+							280
+						],
+						"media_url": "http://pbs.twimg.com/media/FRIGj85aUAAZIpd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FRIGj85aUAAZIpd.jpg",
+						"url": "https://t.co/bb0WtmFOnH",
+						"display_url": "pic.twitter.com/bb0WtmFOnH",
+						"expanded_url": "https://twitter.com/space_doge_coin/status/1518283254734127104/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1518283239089393700,
+						"id_str": "1518283239089393664",
+						"indices": [
+							257,
+							280
+						],
+						"media_url": "http://pbs.twimg.com/media/FRIGj85aUAAZIpd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FRIGj85aUAAZIpd.jpg",
+						"url": "https://t.co/bb0WtmFOnH",
+						"display_url": "pic.twitter.com/bb0WtmFOnH",
+						"expanded_url": "https://twitter.com/space_doge_coin/status/1518283254734127104/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1515720687964803000,
+				"id_str": "1515720687964803075",
+				"name": "spacedogecoin",
+				"screen_name": "space_doge_coin",
+				"location": "",
+				"description": "Get on board, Elon Musk!\nPlease message me: I will send 50% SPACEDOGE coin",
+				"url": "https://t.co/AtAT80iEUO",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/AtAT80iEUO",
+								"expanded_url": "http://space-doge-coin.com",
+								"display_url": "space-doge-coin.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16719,
+				"friends_count": 1,
+				"listed_count": 0,
+				"created_at": "Sun Apr 17 15:56:03 +0000 2022",
+				"favourites_count": 20,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 41,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1518281183389687808/L68OKlZ8_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1518281183389687808/L68OKlZ8_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1515720687964803075/1650821702",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3992,
+			"favorite_count": 4189,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@westiant @balicantik @jangopramartha \nIkutan Airdrops kuy... \nWe were born to travel to the moon https://t.co/palYkXV7BG"
+	},
+	{
 		"created_at": "Fri May 06 02:30:55 +0000 2022",
 		"id": 1522403507118428200,
 		"id_str": "1522403507118428160",
