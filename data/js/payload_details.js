5ca62403a81fa8df31ca692837f064fe84be59cd
kulkul <e_bowis@yahoo.com>
2022-06-03 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9779,
-	"created_at": "Tue May 24 01:14:51 +0000 2022",
+	"tweets": 9780,
+	"created_at": "Fri Jun 03 01:14:51 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_06.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2022_06 = 
+[
+	{
+		"created_at": "Thu Jun 02 04:19:50 +0000 2022",
+		"id": 1532215390289375200,
+		"id_str": "1532215390289375232",
+		"full_text": "Lagi ngetes platform streaming musik online terbaru. Ada fitur kita dibayar $$ dari tiap lagu yg kita putar, sepertinya bagi hasil dari iklan mungkin juga burn buat ngumpulin pengguna. layak coba nih. Ada streaming radionya. komplit, dari seluruh dunia.\nhttps://t.co/NXF9qyuf0F https://t.co/NUFrNpZkx6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NXF9qyuf0F",
+					"expanded_url": "https://radiofin.com/af/ry2lmm3cthrbmu31",
+					"display_url": "radiofin.com/af/ry2lmm3cthr…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532215170981761000,
+					"id_str": "1532215170981761024",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FUOFlHnVEAAwhg9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUOFlHnVEAAwhg9.jpg",
+					"url": "https://t.co/NUFrNpZkx6",
+					"display_url": "pic.twitter.com/NUFrNpZkx6",
+					"expanded_url": "https://twitter.com/putuebo/status/1532215390289375232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 635,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 558,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532215170981761000,
+					"id_str": "1532215170981761024",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FUOFlHnVEAAwhg9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUOFlHnVEAAwhg9.jpg",
+					"url": "https://t.co/NUFrNpZkx6",
+					"display_url": "pic.twitter.com/NUFrNpZkx6",
+					"expanded_url": "https://twitter.com/putuebo/status/1532215390289375232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 635,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 558,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2294,
+			"friends_count": 1995,
+			"listed_count": 13,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12463,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Lagi ngetes platform streaming musik online terbaru. Ada fitur kita dibayar $$ dari tiap lagu yg kita putar, sepertinya bagi hasil dari iklan mungkin juga burn buat ngumpulin pengguna. layak coba nih. Ada streaming radionya. komplit, dari seluruh dunia.\nhttps://t.co/NXF9qyuf0F https://t.co/NUFrNpZkx6"
+	}
+]
