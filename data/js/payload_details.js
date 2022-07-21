57c57cb4d37ee82344785b3b41f08ecda0f8b9f2
kulkul <e_bowis@yahoo.com>
2022-07-21 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9781,
-	"created_at": "Mon Jun 20 01:14:51 +0000 2022",
+	"tweets": 9782,
+	"created_at": "Thu Jul 21 01:14:51 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,140 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Wed Jul 20 07:42:09 +0000 2022",
+		"id": 1549660920535584800,
+		"id_str": "1549660920535584769",
+		"full_text": "@memer_0001 @CoreHoneycomb @Coredao_Org @CORE_Honeycomb 0xE3490BA3feD52A0fe7BF1cb91E423d852611F48c",
+		"truncated": false,
+		"display_text_range": [
+			56,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "memer_0001",
+					"name": "Force BNB",
+					"id": 1484812806369255400,
+					"id_str": "1484812806369255426",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "CoreHoneycomb",
+					"name": "Satoshi CORE",
+					"id": 1448591149619236900,
+					"id_str": "1448591149619236868",
+					"indices": [
+						12,
+						26
+					]
+				},
+				{
+					"screen_name": "Coredao_Org",
+					"name": "Core DAO",
+					"id": 1526895064869486600,
+					"id_str": "1526895064869486592",
+					"indices": [
+						27,
+						39
+					]
+				},
+				{
+					"screen_name": "CORE_Honeycomb",
+					"name": "Core Honeycomb",
+					"id": 1537688626565619700,
+					"id_str": "1537688626565619712",
+					"indices": [
+						40,
+						55
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1548915017659154400,
+		"in_reply_to_status_id_str": "1548915017659154432",
+		"in_reply_to_user_id": 1484812806369255400,
+		"in_reply_to_user_id_str": "1484812806369255426",
+		"in_reply_to_screen_name": "memer_0001",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2274,
+			"friends_count": 1978,
+			"listed_count": 14,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12432,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "pt",
+		"text": "@memer_0001 @CoreHoneycomb @Coredao_Org @CORE_Honeycomb 0xE3490BA3feD52A0fe7BF1cb91E423d852611F48c"
+	}
+]
