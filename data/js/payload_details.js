f87acdfe83d42b773ca768e86760bb2d688e68e5
kulkul <e_bowis@yahoo.com>
2022-12-12 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9793,
-	"created_at": "Sun Dec 04 01:14:51 +0000 2022",
+	"tweets": 9794,
+	"created_at": "Mon Dec 12 01:14:51 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Sun Dec 11 13:09:26 +0000 2022",
+		"id": 1601626185464938500,
+		"id_str": "1601927138931712000",
+		"full_text": "https://t.co/XhnRJPLlOY https://t.co/ZkQRptLY3r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XhnRJPLlOY",
+					"expanded_url": "https://twitter.com/akbarfaizal68/status/1601224317265928193",
+					"display_url": "twitter.com/akbarfaizal68/…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1601626183124148200,
+					"id_str": "1601626183124148225",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FjoehxwUYAErX9Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjoehxwUYAErX9Y.jpg",
+					"url": "https://t.co/ZkQRptLY3r",
+					"display_url": "pic.twitter.com/ZkQRptLY3r",
+					"expanded_url": "https://twitter.com/Sorasachi__/status/1601626185464938498/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1601626183124148200,
+					"id_str": "1601626183124148225",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FjoehxwUYAErX9Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjoehxwUYAErX9Y.jpg",
+					"url": "https://t.co/ZkQRptLY3r",
+					"display_url": "pic.twitter.com/ZkQRptLY3r",
+					"expanded_url": "https://twitter.com/Sorasachi__/status/1601626185464938498/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 425,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1132743854589792300,
+			"id_str": "1132743854589792256",
+			"name": "𝚉𝚎𝚛𝚔𝚊_𝚂𝚊𝚌𝚑𝚒™",
+			"screen_name": "Sorasachi__",
+			"location": "Sofa samping jendela",
+			"description": "~ Maddah ~\n\n“Kenalilah apa yang ada di hadapanmu, dan apa yang disembunyikan darimu akan menjadi jelas bagimu.”",
+			"url": "https://t.co/I8B75ABer9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/I8B75ABer9",
+							"expanded_url": "http://sachimoonsky.blogspot.com",
+							"display_url": "sachimoonsky.blogspot.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4960,
+			"friends_count": 5310,
+			"listed_count": 0,
+			"created_at": "Sun May 26 20:22:35 +0000 2019",
+			"favourites_count": 62719,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 54275,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1530809398112976897/jpLysj0p_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1530809398112976897/jpLysj0p_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1132743854589792256/1630587555",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1601224317265928200,
+		"quoted_status_id_str": "1601224317265928193",
+		"quoted_status_permalink": {
+			"url": "https://t.co/XhnRJPLlOY",
+			"expanded": "https://twitter.com/akbarfaizal68/status/1601224317265928193",
+			"display": "twitter.com/akbarfaizal68/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Dec 09 14:36:41 +0000 2022",
+			"id": 1601224317265928200,
+			"id_str": "1601224317265928193",
+			"full_text": "Bpk sy operasi militer di Irian Barat/Papua. Lengannya bengkok kena peluru. Operasi di Tim-Tim/Timor Leste. Dpt Bintang Seroja. Hingga wafat berpangkat Sertu. Kami bangga padanya. Dan, seorg pesohor tiba2 dpt pangkat Letkol dari Menhan Prabowo, panglima TNI &amp; Kasad. Negeri suka2.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				284
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 126260062,
+				"id_str": "126260062",
+				"name": "Akbar Faizal",
+				"screen_name": "akbarfaizal68",
+				"location": "ÜT: -6.218719,106.808403",
+				"description": "https://t.co/DI3Bqw8lJz",
+				"url": "https://t.co/cqfxbbLhIe",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/cqfxbbLhIe",
+								"expanded_url": "http://www.akbarfaizal.com",
+								"display_url": "akbarfaizal.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/DI3Bqw8lJz",
+								"expanded_url": "http://bit.ly/AFUncensored",
+								"display_url": "bit.ly/AFUncensored",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 188758,
+				"friends_count": 194,
+				"listed_count": 174,
+				"created_at": "Thu Mar 25 08:35:06 +0000 2010",
+				"favourites_count": 41,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 9890,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B2DFDA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1265352970582978561/KWfRKSG1_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1265352970582978561/KWfRKSG1_normal.jpg",
+				"profile_link_color": "93A644",
+				"profile_sidebar_border_color": "FFA70F",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3428,
+			"favorite_count": 12740,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "in"
+		},
+		"retweet_count": 6,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/XhnRJPLlOY https://t.co/ZkQRptLY3r"
+	},
+	{
 		"created_at": "Sat Dec 03 05:43:09 +0000 2022",
 		"id": 1598915721261875200,
 		"id_str": "1598915721261875200",
