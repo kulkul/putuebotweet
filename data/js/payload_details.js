f1d27660b087849c5bd42ea838771b537ea35e28
kulkul <e_bowis@yahoo.com>
2023-02-27 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9817,
-	"created_at": "Mon Jan 09 01:14:51 +0000 2023",
+	"tweets": 9818,
+	"created_at": "Mon Feb 27 01:14:51 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_02.js
@@ -0,0 +1,120 @@
+Grailbird.data.tweets_2023_02 = 
+[
+	{
+		"created_at": "Sun Feb 26 06:11:05 +0000 2023",
+		"id": 1629725720342831000,
+		"id_str": "1629725720342831104",
+		"full_text": "@sandrasetiadi Hai @sandrasetiadi .. Terimakasih..:) Mari bertumbuh bersama. Sehat. damai dan senantiasa berkelimpahan...",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			121
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sandrasetiadi",
+					"name": "sandra setiadi",
+					"id": 72271486,
+					"id_str": "72271486",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "sandrasetiadi",
+					"name": "sandra setiadi",
+					"id": 72271486,
+					"id_str": "72271486",
+					"indices": [
+						19,
+						33
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1623321029467689000,
+		"in_reply_to_status_id_str": "1623321029467688962",
+		"in_reply_to_user_id": 72271486,
+		"in_reply_to_user_id_str": "72271486",
+		"in_reply_to_screen_name": "sandrasetiadi",
+		"user": {
+			"id": 14065297,
+			"id_str": "14065297",
+			"name": "Putu Ebo",
+			"screen_name": "putuebo",
+			"location": "Bali",
+			"description": "Half Cartoonist, Half Jedi. Full timer cinta NKRI",
+			"url": "http://t.co/bHuYxX1W2B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bHuYxX1W2B",
+							"expanded_url": "http://putuebo.com",
+							"display_url": "putuebo.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2233,
+			"friends_count": 1982,
+			"listed_count": 12,
+			"created_at": "Sat Mar 01 16:05:55 +0000 2008",
+			"favourites_count": 12530,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9828,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F8FF13",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/490531049705713664/lVtgvmY7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14065297/1411580550",
+			"profile_link_color": "A60A0A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DE5E11",
+			"profile_text_color": "FFD583",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "in",
+		"text": "@sandrasetiadi Hai @sandrasetiadi .. Terimakasih..:) Mari bertumbuh bersama. Sehat. damai dan senantiasa berkelimpahan..."
+	}
+]
