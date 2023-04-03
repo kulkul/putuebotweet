c68a0dcd2b58f77eb05d9ea2aae1d7656e969987
kulkul <e_bowis@yahoo.com>
2023-04-03 09:14:52 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9818,
-	"created_at": "Mon Feb 27 01:14:51 +0000 2023",
+	"tweets": 9819,
+	"created_at": "Mon Apr 03 01:14:51 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,196 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Sun Apr 02 05:49:48 +0000 2023",
+		"id": 1642329264564609000,
+		"id_str": "1642403940045328385",
+		"full_text": "📉The 1st of each month is the day when #PiNetwork’s basic computing power drops. At present, the new basic mining computing power is 0.0115π/h×24/h=0.276π/day, and a maximum of 8.28π can be mined this month. With the participation of more active pioneers,  Pi will become more… https://t.co/iTLef5SlmF https://t.co/pJlO9vHr75",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			301
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PiNetwork",
+					"indices": [
+						39,
+						49
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iTLef5SlmF",
+					"expanded_url": "https://twitter.com/i/web/status/1642329264564609026",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						278,
+						301
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1642329261448233000,
+					"id_str": "1642329261448232963",
+					"indices": [
+						302,
+						325
+					],
+					"media_url": "http://pbs.twimg.com/media/Fsq5wwjaAAMH_XY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fsq5wwjaAAMH_XY.jpg",
+					"url": "https://t.co/pJlO9vHr75",
+					"display_url": "pic.twitter.com/pJlO9vHr75",
+					"expanded_url": "https://twitter.com/PiNewsMedia/status/1642329264564609026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 771,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 771,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1642329261448233000,
+					"id_str": "1642329261448232963",
+					"indices": [
+						302,
+						325
+					],
+					"media_url": "http://pbs.twimg.com/media/Fsq5wwjaAAMH_XY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fsq5wwjaAAMH_XY.jpg",
+					"url": "https://t.co/pJlO9vHr75",
+					"display_url": "pic.twitter.com/pJlO9vHr75",
+					"expanded_url": "https://twitter.com/PiNewsMedia/status/1642329264564609026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 771,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 771,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1313548940944396300,
+			"id_str": "1313548940944396288",
+			"name": "Pi News",
+			"screen_name": "PiNewsMedia",
+			"location": "",
+			"description": "Pi News is the leading media platform on Pi Network.  Designed to analyze and track project progress, build the Pi ecosystem and nurture the Pi community.",
+			"url": "https://t.co/vLLFyRTY0F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vLLFyRTY0F",
+							"expanded_url": "http://minepi.com",
+							"display_url": "minepi.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 604045,
+			"friends_count": 56,
+			"listed_count": 223,
+			"created_at": "Tue Oct 06 18:37:40 +0000 2020",
+			"favourites_count": 949,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1612176325619613697/RgjJX0uo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1612176325619613697/RgjJX0uo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1313548940944396288/1673962549",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 221,
+		"favorite_count": 1496,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "📉The 1st of each month is the day when #PiNetwork’s basic computing power drops. At present, the new basic mining computing power is 0.0115π/h×24/h=0.276π/day, and a maximum of 8.28π can be mined this month. With the participation of more active pioneers,  Pi will become more… https://t.co/iTLef5SlmF https://t.co/pJlO9vHr75"
+	}
+]
